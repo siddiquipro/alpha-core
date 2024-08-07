0c85b93394c848fac2f1e197efec83b49b8f04c0
Mohammad Siddiqui <masiddiqui91@gmail.com>
2024-08-07 14:37:34 +0100

    start command

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
+import { randomUUID } from "crypto";
 import { Hono } from "hono";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.json({ message: "Hello Hono!", date: new Date() });
+});
+
+app.get("/users", (c) => {
+  const users = Array.from({ length: 100 }).map((_, i) => ({
+    id: randomUUID(),
+    name: `User ${i + 1}`,
+    email: `user-${i + 1}@example.com`,
+  }));
+  return c.json(users);
 });
 
 export default app;
